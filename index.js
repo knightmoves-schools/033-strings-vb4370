cb27a0f6aec7618b651119a7036aa422833c2818
vb4370 <victoria.bigeagle@students.msswarriors.org>
2026-01-27 20:43:52 +0000

    .

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let firstName = "idk" ;
 let jobTitle = "idk" ;
 let city = "idk" ;
 
-let newJobPost = "${firstName} ${jobTitle} ${city}"
+let newJobPost = "${firstName}, ${jobTitle}, ${city}"
 
 document.getElementById('result').innerHTML = newJobPost;
