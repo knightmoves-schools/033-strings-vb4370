2146875a4dd4021b2f356cdbf7a18c3241b29218
vb4370 <victoria.bigeagle@students.msswarriors.org>
2026-01-27 20:24:28 +0000

    .

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ let firstName = "idk" ;
 let jobTitle = "idk" ;
 let city = "idk" ;
 
-let newJobPost = "firstName" ;
-let newJobPost = "jobTitle" ;
-let newJobPost = "city" ;
-
 document.getElementById('result').innerHTML = newJobPost;
